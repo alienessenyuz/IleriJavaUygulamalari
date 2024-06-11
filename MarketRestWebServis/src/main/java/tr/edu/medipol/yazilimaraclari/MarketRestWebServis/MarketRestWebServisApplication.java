ba8bf9191f74c412f62a22fb2a5f5131577c1d55
alienessenyuz <ali.senyuz@std.medipol.edu.tr.>
2024-06-11 23:52:40 +0300

    Proje eklendi

---
+++ MarketRestWebServis/src/main/java/tr/edu/medipol/yazilimaraclari/MarketRestWebServis/MarketRestWebServisApplication.java
@@ -0,0 +1,25 @@
+package tr.edu.medipol.yazilimaraclari.MarketRestWebServis; 
+
+ 
+
+import org.springframework.boot.SpringApplication; 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+ 
+
+@SpringBootApplication 
+
+public class MarketRestWebServisApplication { 
+
+ 
+
+    public static void main(String[] args) { 
+
+        SpringApplication.run(MarketRestWebServisApplication.class, args); 
+
+    } 
+
+} 
+
+ 
---
+++ MarketRestWebServis/src/main/java/tr/edu/medipol/yazilimaraclari/MarketRestWebServis/Urun.java
@@ -0,0 +1,41 @@
+package tr.edu.medipol.yazilimaraclari.MarketRestWebServis; 
+
+ 
+
+public class Urun { 
+
+ 
+
+    private String urunAdi; 
+
+ 
+
+    public Urun() { 
+
+    } 
+
+ 
+
+    public Urun(String urunAdi) { 
+
+        this.urunAdi = urunAdi; 
+
+    } 
+
+ 
+
+    public String getUrunAdi() { 
+
+        return urunAdi; 
+
+    } 
+
+ 
+
+    public void setUrunAdi(String urunAdi) { 
+
+        this.urunAdi = urunAdi; 
+
+    } 
+
+} 
